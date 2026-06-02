924bdd61a36d74a74aa918dd5f746c887656d1a9
MdSaifAli063 <mdsaifali6303@gmail.com>
2026-06-03 01:58:48 +0530

    added

--- src/components/ui/BrandLogo.jsx
+++ src/components/ui/BrandLogo.jsx
@@ -2,13 +2,13 @@ import clsx from 'clsx';
 import { Link } from 'react-router-dom';
 
 // Put your logo image in public/stc-logo.png, or replace this with a hosted image URL.
-const LOGO_SRC = 'https://ik.imagekit.io/77nsbwefl/file_000000006cc4720690091afb149e1eedremove.png';
+const LOGO_SRC = 'https://ik.imagekit.io/77nsbwefl/1000105108.png';
 
 export default function BrandLogo({
   className = '',
   textClassName = '',
   imgClassName = '',
-  iconSize = 50,
+  iconSize = 60,
   asLink = true,
 }) {
   const content = (
